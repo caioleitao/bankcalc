5fdc6a7e4fd8fbc8ef06e8aec0fcecf63e09ed52
Caio César <br.caiorecife@cesarleitao.com.br>
2019-07-28 18:23:35 -0300

    files

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,3 +1,12 @@
 function check(){
-    window.alert("Its work!")
+    var capital = parseFloat(document.getElementById("capital").value)
+    var tax = parseFloat(document.getElementById("tax").value)
+    var time = parseFloat(document.getElementById("time").value)
+    var amount = document.getElementById("amount")
+
+    var percentage = (capital * tax) / 100
+
+    calc = (percentage * time) + capital
+
+    amount.innerHTML = `Amount: R$${calc}`
 }
