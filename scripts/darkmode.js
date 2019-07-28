c91a7cec08288cc853d048b9840aa8748831f986
Caio César <br.caiorecife@cesarleitao.com.br>
2019-07-28 20:13:59 -0300

    Update darkmode.js

--- scripts/darkmode.js
+++ scripts/darkmode.js
@@ -1,3 +1,25 @@
 function darkmode(){
+    var colorbody = document.getElementById("colorbody")
+    var color = document.getElementById("color")
+    var night = document.getElementById("nightmodeimg")
+    var sun = document.getElementById("sunmodeimg")
 
+    colorbody.style.backgroundColor = "#272727"
+    color.style.backgroundColor = "#121212"
+    night.style.display = "none"
+    sun.style.display = "block"
+    
+}
+
+function lightmode(){
+    var colorbody = document.getElementById("colorbody")
+    var color = document.getElementById("color")
+    var night = document.getElementById("nightmodeimg")
+    var sun = document.getElementById("sunmodeimg")
+
+    colorbody.style.backgroundColor = "#ddd"
+    color.style.backgroundColor = "#fff"
+    night.style.display = "block"
+    sun.style.display = "none"
+    
 }
